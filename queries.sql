dd858a8c072d058faa82f3d5f279ca5e6a87159d
Namom Alencar <namomalencar@gmail.com>
2017-10-02 21:16:05 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,6 @@ where
   and o.o_orderdate<'1994-01-01'
   and l.l_returnflag ='R' 
   and l.l_orderkey = o.o_orderkey;
+
+
+select c_custkey, n_nationkey from customer, nation where c_nationkey = n_nationkey;
